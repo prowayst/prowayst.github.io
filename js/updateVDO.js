5703be852b89846f9bfa6cd2c4efc3cc73bbb9c5
mhl916 <haulamma@gmail.com>
2024-06-18 17:44:42 +0800

    2

--- js/updateVDO.js
+++ js/updateVDO.js
@@ -1,21 +1,32 @@
-const loadVideo = (iframe) => {
-    const cid = "UCVtCLmxm96m3tgEEQOG1raQ";
-    const channelURL = encodeURIComponent(`https://www.youtube.com/feeds/videos.xml?channel_id=${cid}`)
-    const reqURL = `https://api.rss2json.com/v1/api.json?rss_url=${channelURL}`;
+const loadVideo = async (iframe) => {
+    const cid = "UCVtCLmxm96m3tgEEQOG1raQ"; 
+    const channelURL = encodeURIComponent(`https://www.youtube.com/feeds/videos.xml?channel_id=${cid}`);
+    const proxyURL = `https://api.allorigins.win/get?url=${channelURL}`;
 
-    fetch(reqURL)
-        .then(response => response.json())
-        .then(result => {
-          console.log(result)
-            const videoNumber = iframe.getAttribute('vnum')
-            const link = result.items[videoNumber].link;
-            const id = link.substr(link.indexOf("=") + 1);
-            iframe.setAttribute("src", `https://youtube.com/embed/${id}?controls=0&autoplay=1`);
-        })
-        .catch(error => console.log('error', error));
-}
+    try {
+        const response = await fetch(proxyURL);
+        if (!response.ok) {
+            throw new Error('Network response was not ok ' + response.statusText);
+        }
+        const data = await response.json();
+        const xmlText = data.contents;
+        const parser = new DOMParser();
+        const xmlDoc = parser.parseFromString(xmlText, "application/xml");
+        
+        const entries = xmlDoc.getElementsByTagName('entry');
+        const videoNumber = parseInt(iframe.getAttribute('vnum')); 
+        if (videoNumber >= entries.length) {
+            throw new Error('Invalid video number');
+        }
+
+        const videoId = entries[videoNumber].getElementsByTagName('yt:videoId')[0].textContent;
+        iframe.setAttribute("src", `https://www.youtube.com/embed/${videoId}?controls=0&autoplay=1`);
+    } catch (error) {
+        console.error('Fetch error: ', error);
+    }
+};
 
 const iframes = document.getElementsByClassName('latestVideoEmbed');
-for (let i = 0, len = iframes.length; i < len; i++) {
+for (let i = 0; i < iframes.length; i++) {
     loadVideo(iframes[i]);
 }
